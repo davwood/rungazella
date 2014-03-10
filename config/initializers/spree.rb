6d072e3bc93d1a0ef90fd2efc64e4e7a64592f7f
davewood <davwood_1@yahoo.com.au>
2014-03-10 17:08:40 +1100

    changed logo location to images/store and added some home page content

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,8 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  config.site_name = "Gazella Running Costumes"
+  config.logo = "store/rungazella.png"
 end
 
 Spree.user_class = "Spree::User"
