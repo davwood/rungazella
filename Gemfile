0650dff2e07536b4bc286aa676af29cc410b1c9a
davewood <davwood_1@yahoo.com.au>
2014-03-17 10:40:26 +1100

    added blogging functionality with the spree-blogging-spree gem

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ gem 'spree_gateway', :git => 'https://github.com/spree/spree_gateway.git', :bran
 gem 'spree_auth_devise', :git => 'https://github.com/spree/spree_auth_devise.git', :branch => '2-2-stable'
 gem 'spree', '2.2.0'
 gem 'spree_rungazella_bootstrap', :path => "./spree_rungazella_bootstrap"
+gem 'spree_blogging_spree', :path => "./spree-blogging-spree"
 gem 'kaminari-bootstrap', '~> 3.0.1'
 
 gem 'rails_12factor', group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311211168) do
+ActiveRecord::Schema.define(version: 20140316233458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,20 @@ ActiveRecord::Schema.define(version: 20140311211168) do
 
   add_index "spree_assets", ["viewable_id"], name: "index_assets_on_viewable_id", using: :btree
   add_index "spree_assets", ["viewable_type", "type"], name: "index_assets_on_viewable_type_and_type", using: :btree
+
+  create_table "spree_blog_entries", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "permalink"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "visible",      default: false
+    t.datetime "published_at"
+    t.text     "summary"
+    t.integer  "author_id"
+  end
+
+  add_index "spree_blog_entries", ["author_id"], name: "index_spree_blog_entries_on_author_id", using: :btree
 
   create_table "spree_calculators", force: true do |t|
     t.string   "type"
@@ -731,6 +745,10 @@ ActiveRecord::Schema.define(version: 20140311211168) do
     t.datetime "updated_at"
     t.datetime "remember_created_at"
     t.string   "spree_api_key",          limit: 48
+    t.string   "nickname"
+    t.string   "website_url"
+    t.string   "google_plus_url"
+    t.text     "bio_info"
   end
 
   add_index "spree_users", ["email"], name: "email_idx_unique", unique: true, using: :btree
@@ -772,6 +790,23 @@ ActiveRecord::Schema.define(version: 20140311211168) do
     t.integer  "zone_members_count", default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string "name"
   end
 
 end
