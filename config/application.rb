26a5cf8c7450220fd3f6f6e9f64ad037058d840a
davewood <davwood_1@yahoo.com.au>
2014-07-21 20:07:06 +1000

    precompile stripe.js

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Rungazella
 
     #add the fonts path
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-
+    config.assets.precompile += %w(store/gateway/stripe.js)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
