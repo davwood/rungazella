f5a3f40e4ff9397e83df02b6e9f52d0a3cb95538
Dave Wood <dave.wood@blake.com.au>
2015-05-31 20:25:21 +1000

    typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :repo_url, 'git@github.com:davwood/rungazella.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :linked_files, fetch(:linked_files, []).push('config/database.yml, config/local_env.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/local_env.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 after 'deploy:publishing', 'deploy:restart'
