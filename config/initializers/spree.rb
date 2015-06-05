15f0114c741c6ce376980a07b7304fadcc43aa25
Dave Wood <dave.wood@blake.com.au>
2015-06-05 14:35:57 +1000

    bucket is not secret

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -7,65 +7,38 @@
 # config.setting_name = 'new value'
 
 Spree.config do |config|
-  	# Example:
-  	# Uncomment to override the default site name.
-  	config.site_name = "Gazella Running Costumes"
-  	config.logo = "store/rungazella.png"
+	# Example:
+	# Uncomment to override the default site name.
+	config.site_name = "Gazella Running Costumes"
+	config.logo = "store/rungazella.png"
 
-  	#initial setup
-  	config.allow_ssl_in_production = true
-  	# config.allow_ssl_in_production = false
+	#initial setup
+	config.allow_ssl_in_production = true
+	# config.allow_ssl_in_production = false
 
-  	#S3 configuration
- 	if Rails.env.production? then
+	#S3 configuration
+		#production. Store images on S3.
+		# development will default to local storage
+	attachment_config = {
+	s3_credentials: {
+	  access_key_id: ENV["S3_KEY"],
+	  secret_access_key: ENV["S3_SECRET"],
+	  bucket: 'rungazella',
+	},
 
- 		#production. Store images on S3.
- 		# development will default to local storage
-		attachment_config = {
-		s3_credentials: {
-		  access_key_id: ENV["S3_KEY"],
-		  secret_access_key: ENV["S3_SECRET"],
-		  bucket: ENV["S3_BUCKET"],
-		},
+	storage:        :s3,
+	s3_headers:     { "Cache-Control" => "max-age=31557600" },
+	s3_protocol:    "https",
+	bucket:         'rungazella',
 
-		storage:        :s3,
-		s3_headers:     { "Cache-Control" => "max-age=31557600" },
-		s3_protocol:    "https",
-		bucket:         ENV["S3_BUCKET"],
+	path:          "/:class/:attachment/:id/:style/:basename.:extension",
+	default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
+	default_style: "product",
+	}
 
-		path:          "/:class/:attachment/:id/:style/:basename.:extension",
-		default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
-		default_style: "product",
-		}
-
-		attachment_config.each do |key, value|
-			Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-		end
-
-	elsif Rails.env.development? then
-
-		attachment_config = {
-		s3_credentials: {
-		  access_key_id: ENV["S3_KEY"],
-		  secret_access_key: ENV["S3_SECRET"],
-		  bucket: "rungazella-dev",
-		},
-
-		storage:        :s3,
-		s3_headers:     { "Cache-Control" => "max-age=31557600" },
-		s3_protocol:    "https",
-		bucket:         "rungazella-dev",
-
-		path:          "/:class/:attachment/:id/:style/:basename.:extension",
-		default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
-		default_style: "product",
-		}
-
-		attachment_config.each do |key, value|
-			Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-		end
+	attachment_config.each do |key, value|
+		Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
 	end
-
 end
 
 Spree.user_class = "Spree::User"
