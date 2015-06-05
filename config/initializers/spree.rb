bef0c36c8eeb77a44894fff2196d272b1ce28639
Eugene bezludny <bezludny@gmail.com>
2015-06-05 23:49:34 +0300

    Test S3 buckets

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -23,13 +23,13 @@ Spree.config do |config|
 	s3_credentials: {
 	  access_key_id: ENV["S3_KEY"],
 	  secret_access_key: ENV["S3_SECRET"],
-	  bucket: 'rungazella',
+	  bucket: 'rungazella-not-exist',
 	},
 
 	storage:        :s3,
 	s3_headers:     { "Cache-Control" => "max-age=31557600" },
 	s3_protocol:    "https",
-	bucket:         'rungazella',
+	bucket:         'rungazella-not-exist-2',
 
 	url: 					 ":s3_domain_url",
 	
